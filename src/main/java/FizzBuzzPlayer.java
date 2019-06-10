0ef70415c5dfe19e5f75c123758c7ef9fb7f0f5f
Yapeng ZHANG <ypzhang@thoughtworks.com>
2019-06-10 10:44:09 +0800

    Initial commit

---
+++ src/main/java/FizzBuzzPlayer.java
@@ -0,0 +1,13 @@
+public class FizzBuzzPlayer {
+
+    public String say(int i) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        FizzBuzzPlayer player = new FizzBuzzPlayer();
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(player.say(i));
+        }
+    }
+}
