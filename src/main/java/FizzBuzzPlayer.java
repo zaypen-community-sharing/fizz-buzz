85e9e1249ba50e12df2651ec9abc5b7b9d0da23d
Yapeng ZHANG <ypzhang@thoughtworks.com>
2019-06-10 12:54:21 +0800

    Use StringBuilder for FizzBuzzPlayer.say

--- src/main/java/FizzBuzzPlayer.java
+++ src/main/java/FizzBuzzPlayer.java
@@ -2,17 +2,16 @@ public class FizzBuzzPlayer {
 
     private static final String BUZZ = "Buzz";
     private static final String FIZZ = "Fizz";
-    private static final String FIZZ_BUZZ = FIZZ + BUZZ;
 
     public String say(int i) {
-        if (i % 15 == 0) {
-            return FIZZ_BUZZ;
-        } else if (i % 3 == 0) {
-            return FIZZ;
-        } else if (i % 5 == 0) {
-            return BUZZ;
+        StringBuilder sb = new StringBuilder();
+        if (i % 3 == 0) {
+            sb.append(FIZZ);
         }
-        return String.valueOf(i);
+        if (i % 5 == 0) {
+            sb.append(BUZZ);
+        }
+        return sb.length() > 0 ? sb.toString() : String.valueOf(i);
     }
 
     public static void main(String[] args) {
