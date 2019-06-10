8f23df015cff87d0d23c1da70496b4a43e51e6af
Yapeng ZHANG <ypzhang@thoughtworks.com>
2019-06-10 10:54:33 +0800

    Add testing of say 2

--- src/test/java/FizzBuzzPlayerTest.java
+++ src/test/java/FizzBuzzPlayerTest.java
@@ -9,4 +9,9 @@ public class FizzBuzzPlayerTest {
     public void shouldReturn1WhenSay1() {
         assertEquals("1", player.say(1));
     }
+
+    @Test
+    public void shouldReturn2WhenSay2() {
+        assertEquals("2", player.say(2));
+    }
 }
